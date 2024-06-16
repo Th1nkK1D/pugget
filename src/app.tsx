0ae9d734ead20d62107833b3d0de2c5dae0adf1b
Th1nkK1D <witheep@gmail.com>
2024-06-16 23:58:36 +0700

    chore: setup tailwind and daisy ui

--- src/app.tsx
+++ src/app.tsx
@@ -1,24 +1,25 @@
-import viteLogo from '/vite.svg';
 import { createSignal } from 'solid-js';
+import viteLogo from '/vite.svg';
 
 function App() {
 	const [count, setCount] = createSignal(0);
 
 	return (
-		<>
+		<main class="flex h-dvh flex-col items-center justify-center gap-6">
 			<div>
 				<img src={viteLogo} class="logo" alt="Vite logo" />
 			</div>
 			<h1>Vite + Solid</h1>
-			<div>
-				<button onClick={() => setCount((count) => count + 1)}>
-					count is {count()}
-				</button>
-				<p>
-					Edit <code>src/App.tsx</code> and save to test HMR
-				</p>
-			</div>
-		</>
+			<button
+				class="btn btn-primary"
+				onClick={() => setCount((count) => count + 1)}
+			>
+				count is {count()}
+			</button>
+			<p>
+				Edit <code>src/App.tsx</code> and save to test HMR
+			</p>
+		</main>
 	);
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 /* @refresh reload */
-import App from './app';
 import { render } from 'solid-js/web';
+import App from './app';
+import './index.css';
 
 const root = document.getElementById('root');
 
